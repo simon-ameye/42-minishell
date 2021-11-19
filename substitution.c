50abe8bdd3ee945135352b86f18953499241f759
simon-ameye <ameye.simon@gmail.com>
2021-11-19 17:06:53 +0100

    norm

--- substitution.c
+++ substitution.c
@@ -1,16 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   substitution.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sameye <sameye@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/19 16:18:04 by sameye            #+#    #+#             */
+/*   Updated: 2021/11/19 17:06:26 by sameye           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-/*
-**This version os ft_strjoin is able to join a str to (null)
-*/
-char	*ft_strjoincustomfree(char *s1, char *s2)
+//This version of ft_strjoin is able to
+//join a str to (null) and frees s1  and s2
+void	strjoin_iter(char **str, char **s1, char **s2)
 {
-	char	*str;
-	int		i;
-	int		j;
+	int	i;
+	int	j;
 
 	i = 0;
 	j = 0;
+	while ((*s1)[i] != '\0')
+	{
+		(*str)[i] = (*s1)[i];
+		i++;
+	}
+	while ((*s2)[j] != '\0')
+	{
+		(*str)[i] = (*s2)[j];
+		i++;
+		j++;
+	}
+	(*str)[i] = '\0';
+}
+
+char	*ft_strjoinfree(char *s1, char *s2)
+{
+	char	*str;
+
 	if (!s1)
 		return (NULL);
 	if (!s2)
@@ -18,21 +46,7 @@ char	*ft_strjoincustomfree(char *s1, char *s2)
 	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!(str))
 		return (NULL);
-	while (s1[i] != '\0')
-	{
-		str[i] = s1[i];
-		i++;
-	}
-	if (s2)
-	{
-		while (s2[j] != '\0')
-		{
-			str[i] = s2[j];
-			i++;
-			j++;
-		}
-	}
-	str[i] = '\0';
+	strjoin_iter(&str, &s1, &s2);
 	if (s1)
 		free(s1);
 	if (s2)
@@ -42,7 +56,7 @@ char	*ft_strjoincustomfree(char *s1, char *s2)
 
 char	*joincharfree(char *str, char c)
 {
-	char *res;
+	char	*res;
 
 	if (!str)
 		return (NULL);
@@ -54,116 +68,101 @@ char	*joincharfree(char *str, char c)
 	return (res);
 }
 
-char	*substitution(char *str, char *const *env)
-{
-	int i;
-	int j;
-	char *varname;
-	char *varval;
-	int indbquotes;
-	char *res;
-
-	if (!str || !env)
-		return (NULL);
-	if (!*env)
-		return (ft_strdup(str));
-	res = ft_strdup("");
-	i = 0;
-	indbquotes = 0;
-	while (str[i])
-	{
-		//ft_putstr_fd("current string : ", 2);
-		//ft_putstr_fd(str, 2);
-		//ft_putstr_fd("\n", 2);
-		if (str[i] == '"')
-		{
-			indbquotes = 1 - indbquotes;
-			res = joincharfree(res, str[i]);
-			i++;
-		}
-		else if (str[i] == '\'' && !indbquotes)
-		{
-			res = joincharfree(res, str[i]);
-			i++;
-			while(str[i] && str[i] != '\'')
-			{
-				res = joincharfree(res, str[i]);
-				i++;
-			}
-			if (!str[i])
-			{
-				ft_putstr_fd("Error : simple quote not closed\n", STDERR_FILENO);
-				//free + exit
-			}
-			else
-			{
-				res = joincharfree(res, str[i]);
-				i++;
-			}
-		}
-		else if (str[i] == '$')
-		{
-			//ft_putstr_fd("dollar found\n", 2);
-			i++;
-			j = 0;
-			while (str[j + i] && ft_isalnum(str[j + i]))
-				j++;
-			//printf("i : %d, j : %d, str : %s\n", i, j, str);
-			//fflush(stdout);
-			varname = ft_substr(str, i, j);
-			//printf("varname : '%s'\n", varname);
-			//fflush(stdout);
-			varval = find_var(varname, env);
-			//printf("varval : '%s'\n", varval);
-			//fflush(stdout);
-			if (varname)
-				free(varname);
-			//if (varval)
-			//tmp = strinsertion(str, varval, i, j + 1);
-			/*
-			ft_putstr_fd("before free pb\n", 2);
-			if (str)
-				free(str);
-			ft_putstr_fd("after free pb\n", 2);
-			*/
-			res = ft_strjoincustomfree(res, varval);
-			//free(res);
-			//res = tmp;
-			//str = tmp;
-			i = i + j;
-		}
-		else
-		{
-			res = joincharfree(res, str[i]);
-			i++;
-		}
-	}
-	if (indbquotes)
-		ft_putstr_fd("Error : unexpected EOF while looking for matching `\"\n", STDERR_FILENO);
-		//free + exit
-	return (res);
-}
-
 char	*find_var(char *str, char *const *env)
 {
 	char	*ret;
-	int		len; 
-	
+	int		len;
+
 	ret = NULL;
 	len = ft_strlen(str);
-	//printf("FIND VAR : len : %i\n", len);
-	//fflush(stdout);
-	
 	if (env)
 	{
 		while (*env)
 		{
-			//printf("FIND VAR : tried env %s, str %s, len, %i\n", *env, str, len);
-			//fflush(stdout);
 			if (ft_strnstr(*env, str, len) && *(*env + len) == '=')
 				return (ft_strdup(*env + len + 1));
 			env++;
 		}
 	}
 	return (ret);
+}
+
+void	quotecopy(char **str, char **res)
+{
+	*res = joincharfree(*res, **str);
+	(*str)++;
+	while (**str && **str != '\'')
+	{
+		*res = joincharfree(*res, **str);
+		(*str)++;
+	}
+	if (!**str)
+	{
+		ft_putstr_fd("Error : simple quote not closed\n", STDERR_FILENO);
+		//free + exit
+	}
+	else
+	{
+		*res = joincharfree(*res, **str);
+		(*str)++;
+	}
+}
+
+void	insertenvvar(char **str, char **res, char *const *env)
+{
+	int		len;
+	char	*varname;
+	char	*varval;
+
+	(*str)++;
+	len = 0;
+	while ((*str)[len] && ft_isalnum((*str)[len]))
+		len++;
+	varname = ft_substr(*str, 0, len);
+	varval = find_var(varname, env);
+	if (varname)
+		free(varname);
+	*res = ft_strjoinfree(*res, varval);
+	*str += len;
+}
+
+void	substitutiteration(char **str, char **res,
+	char *const *env, int *indbquotes)
+{
+	if (**str == '"')
+	{
+		*indbquotes = 1 - *indbquotes;
+		*res = joincharfree(*res, **str);
+		(*str)++;
+	}
+	else if (**str == '\'' && !*indbquotes)
+		quotecopy(str, res);
+	else if (**str == '$')
+		insertenvvar(str, res, env);
+	else
+	{
+		*res = joincharfree(*res, **str);
+		(*str)++;
+	}
+}
+
+char	*substitution(char *str, char *const *env)
+{
+	int		indbquotes;
+	char	*res;
+
+	if (!str || !env)
+		return (NULL);
+	if (!*env)
+		return (ft_strdup(str));
+	res = ft_strdup("");
+	indbquotes = 0;
+	while (*str)
+		substitutiteration(&str, &res, env, &indbquotes);
+	if (indbquotes)
+	{
+		ft_putstr_fd("Error : double quote not closed\n", STDERR_FILENO);
+		//	free + exit
+	}
+	return (res);
 }
