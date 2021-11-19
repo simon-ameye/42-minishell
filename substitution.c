f725e4f16b555e119d27006a1cdf402a65138778
simon-ameye <ameye.simon@gmail.com>
2021-11-19 13:15:37 +0100

    add a working substitution BUT LEAKS

--- substitution.c
+++ substitution.c
@@ -1,5 +1,40 @@
 #include "minishell.h"
 
+/*
+**This version os ft_strjoin is able to join a str to (null)
+*/
+char	*ft_strjoincustom(char const *s1, char const *s2)
+{
+	char	*str;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	if (!s1)
+		return (NULL);
+	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!(str))
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	if (s2)
+	{
+		while (s2[j] != '\0')
+		{
+			str[i] = s2[j];
+			i++;
+			j++;
+		}
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+
 char	*strinsertion(char *base, char *add, int pos, int size)
 {
 	char *begin;
@@ -7,16 +42,29 @@ char	*strinsertion(char *base, char *add, int pos, int size)
 	char *res;
 	char *tmp;
 
+	//	printf("STRINSERTION : base %s, add %s, pos %i, size %i\n", base, add, pos, size);
+	//	fflush(stdout);
 	begin = ft_substr(base, 0, pos);
+	//	printf("begin %s\n", begin);
+	//	fflush(stdout);
 	ending = ft_substr(base, pos + size, ft_strlen(base));
-	tmp = ft_strjoin(begin, add);
-	res = ft_strjoin(tmp, ending);
+	//	printf("ending $%s$\n", ending);
+	//	fflush(stdout);
+	tmp = ft_strjoincustom(begin, add);
+	//	printf("tmp %s\n", tmp);
+	//	fflush(stdout);
+	res = ft_strjoincustom(tmp, ending);
+	//	printf("res %s\n", res);
+	//	fflush(stdout);
 	if (begin)
 		free(begin);
 	if (ending)
 		free(ending);
 	if (tmp)
 		free(tmp);
+	
+	//printf("result of strinsertion $%s$\n", res);
+	//	fflush(stdout);
 	return (res);
 }
 
@@ -26,14 +74,19 @@ char	*substitution(char *str, char *const *env)
 	int j;
 	char *varname;
 	char *varval;
+	char *tmp;
+	int indbquotes;
 
 	i = 0;
+	indbquotes = 0;
 	while (str[i])
 	{
-		ft_putstr_fd("current string : ", 2);
-		ft_putstr_fd(str, 2);
-		ft_putstr_fd("\n", 2);
-		if (str[i] == '\'')
+		//ft_putstr_fd("current string : ", 2);
+		//ft_putstr_fd(str, 2);
+		//ft_putstr_fd("\n", 2);
+		if (str[i] == '"')
+			indbquotes = 1 - indbquotes;
+		if (str[i] == '\'' && !indbquotes)
 		{
 			i++;
 			while(str[i] && str[i] != '\'')
@@ -46,22 +99,37 @@ char	*substitution(char *str, char *const *env)
 		}
 		if (str[i] == '$')
 		{
-			ft_putstr_fd("dollar found\n", 2);
-			i++;
+			//ft_putstr_fd("dollar found\n", 2);
+			//i++;
 			j = 0;
-			while (str[j] && ft_isalnum(str[j]))
+			while (str[j + i + 1] && ft_isalnum(str[j + i + 1]))
 				j++;
-			printf("i : %d, j : %d\n", i, j);
-			fflush(stdout);
-			varname = ft_substr(str, 0, i);
+			//printf("i : %d, j : %d, str : %s\n", i, j, str);
+			//fflush(stdout);
+			varname = ft_substr(str, i + 1, j);
+			//printf("varname : '%s'\n", varname);
+			//fflush(stdout);
 			varval = find_var(varname, env);
+			//printf("varval : '%s'\n", varval);
+			//fflush(stdout);
 			if (varname)
 				free(varname);
-			str = strinsertion(str, varval, i, j);
-			i = i + j;
+			//if (varval)
+			tmp = strinsertion(str, varval, i, j + 1);
+			/*
+			ft_putstr_fd("before free pb\n", 2);
+			if (str)
+				free(str);
+			ft_putstr_fd("after free pb\n", 2);
+			*/
+			str = tmp;
+			i = i + j + 1;
 		}
 		i++;
 	}
+	if (indbquotes)
+		ft_putstr_fd("Error : unexpected EOF while looking for matching `\"\n", STDERR_FILENO);
+		//free + exit
 	return (str);
 }
 
@@ -72,23 +140,19 @@ char	*find_var(char *str, char *const *env)
 	
 	ret = NULL;
 	len = ft_strlen(str);
+	//printf("FIND VAR : len : %i\n", len);
+	//fflush(stdout);
+	
 	if (env)
 	{
 		while (*env)
 		{
-			if (ft_strncmp(*env, str, len) && *env[len] == '=')
-				return (ft_strdup(env[len + 1]));
+			//printf("FIND VAR : tried env %s, str %s, len, %i\n", *env, str, len);
+			//fflush(stdout);
+			if (ft_strnstr(*env, str, len) && *(*env + len) == '=')
+				return (ft_strdup(*env + len + 1));
 			env++;
 		}
 	}
 	return (ret);
-}
-
-int	main(int ac, char **av, char **envp)
-{
-	(void)ac;
-	(void)av;
-
-	printf("input :  %s\n", av[1]);
-	printf("result : %s\n", substitution(av[1], envp));
 }
---
+++ testmains/main_substitution.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_substitution.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sameye <sameye@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/19 10:05:54 by sameye            #+#    #+#             */
+/*   Updated: 2021/11/19 13:07:46 by sameye           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//	HOW TO USE :
+//	make -C libft/ && gcc -Wall -Wextra -Werror testmains/main_substitution.c substitution.c -I libft/ -L libft/ -l ft -I. -o main_substitution
+//	export TESTVAR=/cust/path in your shell
+//	./main_substitution
+
+
+
+#include "minishell.h"
+
+void	test(char *str, char * expec, char **envp)
+{	char *res;
+
+	(void)expec;
+	res = NULL;
+	printf("input  : '%s'\n",str);
+	res = substitution(str, envp);
+	printf("result : '%s'\n", res);
+	printf("ref    : '");
+	fflush(stdout);
+	system(str);
+	if (!strcmp(res, expec))
+		printf("'\n\033[0;32mOK\033[0m\n\n");
+	else
+		printf("'\n\033[0;31mNOT OK\033[0m\n\n");
+	fflush(stdout);
+}
+
+int	main(int ac, char **av, char **envp)
+{
+	(void)ac;
+	(void)av;
+
+	//setenv("TESTVAR", "/cust/path", 1);
+
+	test("", "", envp);
+	test("aaaaaaaa", "aaaaaaaa", envp);
+	test("aaa$TESTVAR", "aaa/cust/path", envp);
+	test("$TESTVAR aa", "/cust/path aa", envp);
+	test("aaa$TESTVARR", "aaa", envp);
+	test("aaa$TESTVARR  bb", "aaa  bb", envp);
+	test("aaa$TESTVAR  bb $TESTVAR \"TESTVAR $TESTVAR\" '$TESTVAR'", "aaa/cust/path  bb /cust/path \"TESTVAR /cust/path\" '$TESTVAR'", envp);
+	test(" \" $TESTVAR '$TESTVAR'\" ", " \" /cust/path '/cust/path'\" ", envp);
+	test(" \" aa$TESTVAR '$TESTVAR\" ", " \" aa/cust/path '/cust/path\" ", envp);
+	test("\"", "\"", envp);
+	test("\"'$TESTVAR'\"", "\"'/cust/path'\"", envp);
+	test("'\"$TESTVAR\"'", "'\"$TESTVAR\"'", envp);
+	test("'\"'$TESTVAR'\"'", "'\"'/cust/path'\"'", envp);
+	test("\"'\"$TESTVAR\"'\"", "\"'\"/cust/path\"'\"", envp);
+	test("''\"'\"$TESTVAR\"'\"''", "''\"'\"/cust/path\"'\"''", envp);
+	test("''\"'\"$TESTVAR\"'\"''''", "''\"'\"/cust/path\"'\"''''", envp);
+	test("''\"'\"$TESTVAR\"'\"''''\"", "''\"'\"/cust/path\"'\"''''\"", envp);
+	test(" \"'' ' ' aa$TESTVAR '$TESTVAR\" ' ' ' ", " \"'' ' ' aa/cust/path '/cust/path\" ' ' ' ", envp);
+	test(" \"'' ' '' aa$TESTVAR '$TESTVAR\" ' ' '' ", " \"'' ' '' aa/cust/path '/cust/path\" ' ' '' ", envp);
+	test(" \"'' ' '' aa$TESTVAR '$TESTVAR\" ' ' '' ", " \"'' ' '' aa/cust/path '/cust/path\" ' ' '' ", envp);
+}
