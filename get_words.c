85077785bf564393294a1a46d72470abec677e59
Simon AMEYE <sameye@e2r5p13.clusters.42paris.fr>
2021-11-20 13:08:26 +0100

    nv

--- get_words.c
+++ get_words.c
@@ -6,7 +6,7 @@
 /*   By: sameye <sameye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 11:08:48 by sameye            #+#    #+#             */
-/*   Updated: 2021/11/20 12:29:07 by sameye           ###   ########.fr       */
+/*   Updated: 2021/11/20 13:07:31 by sameye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,39 @@ char	**get_str_words(char *str)
 	int		len;
 	char	**tab;
 	int		i;
+	int		inword;
 
 	len = ft_strlen(str);
 	tab = malloc(sizeof(char *) * (len + 1));
 	sgquote = 0;
 	dbquote = 0;
+	inword = 0;
 
+	str[i] = malloc(sizeof(char));
+	while (str[i])
+	{
+		if (ft_isblank(str[i]) && sgquote == 0 && dbquote == 0)
+		{
+			if (inword == 1)
+				inword = 0;
+			i++;
+		}
+		else if (str[i] == '"' && sgquote == 0)
+		{
+			dbquote = 1 - dbquote;
+			i++;
+		}
+		else if (str[i] == '\'' && dbquote == 0)
+		{
+			sgquote = 1 - sgquote;
+			i++;
+		}
+		else
+		{
+			inword = 1 - inword;
 
+		}
+	}
 
 
 	join_char_free()
