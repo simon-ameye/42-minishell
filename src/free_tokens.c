a21f57af0e80a0937a4c0400b6c61c65edc41248
Simon AMEYE <sameye@e2r12p21.clusters.42paris.fr>
2021-11-22 19:00:12 +0100

    corrected free token

--- src/free_tokens.c
+++ src/free_tokens.c
@@ -10,10 +10,9 @@ void	free_tokens(t_token *tokens)
 		while(!tokens[i].is_last)
 		{
 			if (tokens[i].str)
-			{
 				free(tokens[i].str);
+			if (tokens[i].words)
 				free_str_tab(tokens[i].words);
-			}
 			if (tokens[i].path)
 				free(tokens[i].path);
 			i++;
