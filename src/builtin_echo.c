29a9002af8546697def316325751968b14cece62
Simon AMEYE <sameye@e2r5p1.clusters.42paris.fr>
2021-11-30 11:22:16 +0100

    update echo

--- src/builtin_echo.c
+++ src/builtin_echo.c
@@ -5,35 +5,38 @@ extern unsigned char	g_exitval;
 void	builtin_echo(t_proc *proc)
 {
 	int i;
+	int n_option;
 
+	n_option = 0;
 	if (!proc)
 		return ;
 	i = 0;
 	while (proc->tokens[i].word && proc->tokens[i].type != WORD)
 		i++;
-	if (!proc->tokens[i].word)
-		return ;
-	if (ft_strcmp(proc->tokens[i].word, "-n"))
+	if (proc->tokens[i].word)
 	{
-		ft_putstr_fd("Error: unsupported echo option\n", STDERR_FILENO);
-		return ;
-	}
-	while (proc->tokens[i].word && proc->tokens[i].type == WORD && !ft_strcmp(proc->tokens[i].word, "-n")) //handling multiple "-n"
-		i++;
-	while (proc->tokens[i].word)
-	{
-		if (proc->tokens[i].type == WORD)
-			ft_putstr_fd(proc->tokens[i].word, STDOUT_FILENO);
-		i++;
-	}
-	while (proc->tokens[i].word)
-	{
-		if (proc->tokens[i].type == WORD)
+		while (proc->tokens[i].word && proc->tokens[i].type == WORD && !ft_strcmp(proc->tokens[i].word, "-n")) //handling multiple "-n"
 		{
-			ft_putstr_fd(" ", STDOUT_FILENO);
-			ft_putstr_fd(proc->tokens[i].word, STDOUT_FILENO);
+			n_option = 1;
+			i++;
 		}
-		i++;
+		while (proc->tokens[i].word)
+		{
+			if (proc->tokens[i].type == WORD)
+				ft_putstr_fd(proc->tokens[i].word, STDOUT_FILENO);
+			i++;
+		}
+		while (proc->tokens[i].word)
+		{
+			if (proc->tokens[i].type == WORD)
+			{
+				ft_putstr_fd(" ", STDOUT_FILENO);
+				ft_putstr_fd(proc->tokens[i].word, STDOUT_FILENO);
+			}
+			i++;
+		}
 	}
+	if (!n_option)
+		ft_putstr_fd("\n", STDOUT_FILENO);
 	g_exitval = 0;
 }
