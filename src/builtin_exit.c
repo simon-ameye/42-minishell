1adc3b627ba208dc69141a403f4d105a8e218ea7
Thomas Robin <trobin@student.42.fr>
2021-12-06 13:45:22 +0100

    fix bultin exit

--- src/builtin_exit.c
+++ src/builtin_exit.c
@@ -2,6 +2,7 @@
 
 extern unsigned char	g_exitval;
 
+// move
 static int	get_nb_args(t_token *tokens)
 {
 	int	i;
@@ -21,11 +22,12 @@ static int	get_nb_args(t_token *tokens)
 	return (ret);
 }
 
-static int	custom_ft_atoi(const char *s)
+// move
+static int	ft_atouchar(const char *s)
 {
-	int		i;
-	char	sign;
-	long	result;
+	int	i;
+	int	sign;
+	int	result;
 
 	i = 0;
 	while (s[i] == ' ')
@@ -41,15 +43,52 @@ static int	custom_ft_atoi(const char *s)
 	while (ft_isdigit(s[i]))
 	{
 		result = result * 10 + s[i] - 48;
-		if (result * sign > 2147483647)
-			return (-1);
-		if (result * sign < -2147483648)
-			return (0);
+		if (result * sign < 0 || result * sign > 255)
+			return (-1); // outside uchar range
 		++i;
 	}
-	if (s[i]) // edit here
-			return (-1);
+	if (s[i])
+		return (-2); // wrong numeric format
 	return (result * sign);
+}
+
+static void	exit_too_many_arguments(void)
+{
+	ft_putstr_fd("exit\n", STDERR_FILENO);
+	ft_putstr_fd("minishell: exit: too many arguments\n", STDERR_FILENO);
+	g_exitval = 1;
+}
+
+static void	exit_non_numeric_arguments(t_proc *proc, t_proc *procs)
+{
+	ft_putstr_fd("exit\n", STDERR_FILENO);
+	ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
+	ft_putstr_fd(proc->tokens[1].word, STDERR_FILENO);
+	ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
+	free_env(procs->env);
+	free_procs(procs);
+	g_exitval = 2;
+	exit(g_exitval);
+}
+
+static void	exit_valid_scope(t_proc *proc, t_proc *procs, int exit_value)
+{
+	g_exitval = (unsigned char)exit_value;
+	if (proc->pid)
+		ft_putstr_fd("exit\n", STDERR_FILENO);
+	free_env(procs->env);
+	free_procs(procs);
+	exit(g_exitval);
+}
+
+static void	exit_invalid_scope(t_proc *proc, t_proc *procs)
+{
+	if (proc->pid)
+		ft_putstr_fd("exit\n", STDERR_FILENO);
+	free_env(procs->env);
+	free_procs(procs);
+	g_exitval = 2;
+	exit(g_exitval);
 }
 
 void	builtin_exit(t_proc *proc, t_proc *procs)
@@ -58,55 +97,23 @@ void	builtin_exit(t_proc *proc, t_proc *procs)
 	int	exit_value;
 
 	exit_args = get_nb_args(proc->tokens);
-	//printf("exit_args: %d\n", exit_args);
-
 	if (exit_args == 0)
 	{
-		// don't modify g_exitval
-		ft_putstr_fd("exit\n", STDERR_FILENO);
+		if (proc->pid)
+			ft_putstr_fd("exit\n", STDERR_FILENO);
 		free_env(procs->env);
 		free_procs(procs);
 		exit(g_exitval);
 	}
-	if (exit_args > 0)
+	else
 	{
-		exit_value = custom_ft_atoi(proc->tokens[1].word);
-		// non numeric argument
-		if (exit_value == -1)
-		{
-			ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
-			ft_putstr_fd(proc->tokens[1].word, STDERR_FILENO);
-			ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
-			free_env(procs->env);
-			free_procs(procs);
-			g_exitval = 2;
-			exit(g_exitval);
-		}
-		// too much arguments
+		exit_value = ft_atouchar(proc->tokens[1].word);
+		if (exit_value == -2)
+			return (exit_non_numeric_arguments(proc, procs));
 		if (exit_args > 1)
-		{
-			// don't exit && don't modify g_exitval
-			ft_putstr_fd("exit\n", STDERR_FILENO);
-			ft_putstr_fd("minishell: exit: too many arguments\n", STDERR_FILENO);
-			return ;
-		}
-		// exit_value in correct range
-		if (exit_value > -1 && exit_value < 256)
-		{
-			ft_putstr_fd("exit\n", STDERR_FILENO);
-			free_env(procs->env);
-			free_procs(procs);
-			g_exitval = exit_value;
-			exit(exit_value);
-		}
-		// exit_value in incorrect range
-		else
-		{
-			ft_putstr_fd("exit\n", STDERR_FILENO);
-			free_env(procs->env);
-			free_procs(procs);
-			g_exitval = 42; // define this in header ?
-			exit(g_exitval); // undefined behavior (cf. man exit)
-		}
+			return (exit_too_many_arguments());
+		if (exit_value == -1)
+			return (exit_invalid_scope(proc, procs));
+		return (exit_valid_scope(proc, procs, exit_value));
 	}
 }
