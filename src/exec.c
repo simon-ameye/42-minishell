23a59b75f83c46de3b523473818198a690b521a0
Thomas Robin <trobin@student.42.fr>
2021-12-10 16:19:53 +0100

    proper signals handling

--- src/exec.c
+++ src/exec.c
@@ -2,90 +2,80 @@
 
 extern unsigned char	g_exitval;
 
-static void	create_childs(t_proc *procs)
-{
-	int	i;
-	int	code;
-
-	/*
-	 *	soit:
-	 *	- more than one process
-	 *	- one process not builtin
-	 */
-	if ((procs && !procs[0].is_last && !procs[1].is_last)
-	|| (procs && procs[0].ftype == EXECVE))
-	{
-		i = 0;
-		while (!procs[i].is_last)
-		{
-			procs[i].pid = fork();
-			if (procs[i].pid == -1)
-			{
-				free_procs(procs);
-				exit(EXIT_FAILURE);
-			}
-			if (procs[i].pid == 0)
-			{
-				if (!(procs[i].ftype == NO_FUNCTION || procs[i].ftype == EXIT))
-					g_exitval = 0;
-				close (procs[i].prev_stream_out);
-				close (procs[i].next_stream_in);
-				exec_child(&procs[i], procs);
-				exit(g_exitval); //tmp
-			}
-			if (i > 0)
-				close(procs[i].stream_in);
-			if (!procs[i + 1].is_last)
-				close(procs[i].stream_out);
-			i++;
-		}
-		i = 0;
-		while (!procs[i].is_last)
-		{
-			waitpid(procs[i].pid, &code, 0);
-			if (WIFEXITED(code))
-				g_exitval = WEXITSTATUS(code);
-			i++;
-		}
-		free_procs(procs); //valgrind fix
-	}
-	// one process bultin
-	else
-	{
-		// we are in parent process
-		exec_child(&procs[0], procs);
-	}
-}
-
-static void	create_pipes(t_proc *procs)
+static void	do_pipes(t_proc *procs)
 {
 	int	pipefd[2];
 	int	i;
 
-	// we need at least 2 process
-	if (procs && !procs[0].is_last && !procs[1].is_last)
+	procs[0].stream_in = STDIN_FILENO;
+	i = 1;
+	while (!procs[i].is_last)
 	{
-		procs[0].stream_in = STDIN_FILENO;
-		i = 1;
-		while (!procs[i].is_last)
+		if (pipe(pipefd) == -1)
 		{
-			if (pipe(pipefd) == -1)
-			{
-				free(procs);
-				exit(EXIT_FAILURE);
-			}
-			procs[i].stream_in = pipefd[0];
-			procs[i - 1].stream_out = pipefd[1];
-			procs[i].prev_stream_out =  pipefd[1];
-			procs[i - 1].next_stream_in = pipefd[0];
-			i++;
+			free(procs);
+			exit(EXIT_FAILURE);
 		}
-		procs[i - 1].stream_out = STDOUT_FILENO;
+		procs[i].stream_in = pipefd[0];
+		procs[i - 1].stream_out = pipefd[1];
+		procs[i].prev_stream_out =  pipefd[1];
+		procs[i - 1].next_stream_in = pipefd[0];
+		i++;
 	}
-	else
+	procs[i - 1].stream_out = STDOUT_FILENO;
+}
+
+static void	do_forks(t_proc *procs)
+{
+	int	i;
+
+	i = 0;
+	while (!procs[i].is_last)
 	{
-		procs[0].stream_in = STDIN_FILENO;
-		procs[0].stream_out = STDOUT_FILENO;
+		procs[i].pid = fork();
+		if (procs[i].pid == -1)
+		{
+			free_procs(procs);
+			exit(EXIT_FAILURE);
+		}
+		if (procs[i].pid == 0)
+		{
+			if (!(procs[i].ftype == NO_FUNCTION || procs[i].ftype == EXIT))
+				g_exitval = 0;
+			close(procs[i].prev_stream_out);
+			close(procs[i].next_stream_in);
+			exec_child(&procs[i], procs);
+			exit(g_exitval);
+		}
+		if (i > 0)
+			close(procs[i].stream_in);
+		if (!procs[i + 1].is_last)
+			close(procs[i].stream_out);
+		i++;
+	}
+}
+
+static void	do_waits(t_proc *procs)
+{
+	int	i;
+	int	wstatus;
+
+	i = 0;
+	while (!procs[i].is_last)
+	{
+		signal(SIGINT, SIG_IGN);
+		signal(SIGQUIT, SIG_IGN);
+		if (waitpid(procs[i].pid, &wstatus, 0) == -1)
+			perror(NULL);
+		if (WIFEXITED(wstatus))
+			g_exitval = WEXITSTATUS(wstatus);
+		if (WIFSIGNALED(wstatus))
+		{
+			if (i == 0)
+				write(1, "\n", 1);
+			g_exitval = WTERMSIG(wstatus) + 128;
+		}
+		i++;
 	}
 }
 
@@ -95,7 +85,20 @@ void	exec(t_proc *procs)
 	{
 		// do not forget to close fd
 		// streams are automatically closed by execve
-		create_pipes(procs);
-		create_childs(procs);
+		// cf. valgrind --track-fds=yes
+		if ((procs[0].ftype == EXECVE || !procs[1].is_last))
+		{
+			do_pipes(procs);
+			do_forks(procs);
+			do_waits(procs);
+			init_signals();
+		}
+		else
+		{
+			procs[0].stream_in = STDIN_FILENO;
+			procs[0].stream_out = STDOUT_FILENO;
+			exec_child(&procs[0], procs);
+		}
+		free_procs(procs);
 	}
 }
